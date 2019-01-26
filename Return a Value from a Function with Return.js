137e297a7ba08bee7cd23b95fa33d361ea2df72b
KWFE <berhailriadh@gmail.com>
2019-01-26 13:38:54 -0500

    26-01-2019

---
+++ Return a Value from a Function with Return.js
@@ -0,0 +1,13 @@
+// Example
+function minusSeven(num) {
+    return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num1) {
+
+    return num1 * 5;
+}
+
+console.log(minusSeven(10));
