a467a13aa23f887555340bade2427f04dfada998
KWFE <berhailriadh@gmail.com>
2019-01-25 20:59:03 -0500

    25-01-2019

---
+++ Shopping List.js
@@ -0,0 +1,8 @@
+// the Shopping List made from an array with 5 sub 
+var myList = [
+    ["Computer price in USD", 1500],
+    ["Brush teeth", 1],
+    ["pencils", 15],
+    ["Javel Bottles", 3],
+    ["Riadh's age", 26]
+];
