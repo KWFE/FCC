d980cdaee10e1c997a75dd9a9aec43358ad89fa9
KWFE <berhailriadh@gmail.com>
2019-01-29 20:47:02 -0500

    29-01-2019

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,17 @@
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+    "name": "Kelb",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["mess3ouda", "eliza"]
+
+};
