26e0d1e60f584b931f755c730314f801e39dfa6c
KWFE <berhailriadh@gmail.com>
2019-01-29 22:24:07 -0500

    29-01-2019

---
+++ Accessing Object Properties with Bracket Notation.js
@@ -0,0 +1,9 @@
+// Setup
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "vaggies",
+    "the drink": "water"
+};
+
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
