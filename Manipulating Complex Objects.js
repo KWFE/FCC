9da4d97b71e78866abcb40199b165868b2d0eabd
KWFE <berhailriadh@gmail.com>
2019-01-31 18:39:57 -0500

    31-01-2019

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,26 @@
+var myMusic = [{
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true,
+
+
+    },
+
+    {
+        "artist": "Chikha rimiti",
+        "title": "Ana w Ghzali",
+        "release_year": 1980,
+        "formats": [
+            "DVD",
+            "VCD",
+            "DISQUETTE"
+        ]
+    }
+    // Add record here
+];
